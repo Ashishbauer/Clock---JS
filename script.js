4e28c501997f5b8d0f75f97e2684bf5b7a9e21e0
Ashishbauer <shrivas.ashish2209@gmail.com>
2024-12-14 20:51:57 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+showTime();
+
+setInterval(showTime,1000);
+
+function showTime(){
+    let dt = new Date();
+
+    let h = dt.getHours();
+    let m = dt.getMinutes();
+    let s = dt.getSeconds();
+
+    let hours = document.querySelector("#hours");
+    let minutes = document.querySelector("#minutes");
+    let seconds = document.querySelector("#seconds");
+
+    hours.textContent = h;
+    minutes.textContent = m;
+    seconds.textContent = s;
+}
